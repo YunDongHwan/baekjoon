507fcb3e4bd445981ead56252359935f38530caf
doyun <flfns333@gmail.com>
2023-01-10 15:57:05 +0900

    sort

---
+++ Sort/2587.py
@@ -0,0 +1,17 @@
+def find(num, arr):
+    if len(arr) == 0:
+        return 0
+    for i in range(len(arr)):
+        if arr[i] > num:
+            return i
+    return len(arr)
+
+arr = []
+
+for i in range(5):
+    num = int(input())
+    j = find(num, arr)
+    arr.insert(j, num)
+
+print(sum(arr) // 5)
+print(arr[2])
